7ad9dd988b5950d56f75c98343079b9501fa75fc
Nigel Atkinson <merlinblack@users.noreply.github.com>
2025-09-22 21:33:54 +1000

    Fedora dependancies installation.

---
+++ install-dependancies-fedora.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+sudo dnf install \
+  cmake \
+  gcc-c++ \
+  lua-json \
+  lua-http \
+  lua-bit32 \
+  readline-devel \
+  SDL2_gfx-devel \
+  SDL2_image-devel \
+  SDL2_ttf-devel \
+  SDL2_ttf-devel \
+  sdl2-compat-devel
+
+
+
