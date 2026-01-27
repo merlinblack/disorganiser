0d90fc78a6aa8e4b54aa5acdd0aaf04eb9991c4a
Nigel Atkinson <merlinblack@users.noreply.github.com>
2026-01-27 18:15:13 +1100

    Fix occasional missed chime

--- scripts/clockchimes.lua
+++ scripts/clockchimes.lua
@@ -5,6 +5,8 @@ clockchimesstop = false
 clockchimesvolume = 64
 
 function clockChimesTask()
+	local previouslyTriggered
+
 	while not clockchimesstop do
 		if clockchimes then
 			local seconds = tonumber(os.date '%S')
@@ -13,12 +15,14 @@ function clockChimesTask()
 
 			--print(hour, minutes, seconds)
 
-			if seconds == 0 then
-				if hour == 12 and minutes == 0 then
-					play('ticktokstrike.mp3', clockchimesvolume)
-				else
-					if hour > 5 and hour <= 23 then
-						if minutes == 0 then play('dingdong.mp3', clockchimesvolume) end
+			if seconds < 2 and previouslyTriggered ~= hour then
+				previouslyTriggered = hour
+
+				if minutes == 0 then
+					if hour == 12 then
+						play('ticktokstrike.mp3', clockchimesvolume)
+					else
+						if hour > 5 and hour <= 23 then play('dingdong.mp3', clockchimesvolume) end
 					end
 				end
 			end
